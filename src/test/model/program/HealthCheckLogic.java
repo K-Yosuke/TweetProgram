68a3f12c9ac111bdb01cd389a7e1100c1b3bb144
Kawaguchi, Yosuke <yosuke.kawaguchi.0129@gmail.com>
2015-04-08 22:57:30 +0900

    MVCモデル実装

---
+++ src/test/model/program/HealthCheckLogic.java
@@ -0,0 +1,26 @@
+package test.model.program;
+
+public class HealthCheckLogic {
+  public void execute(Health health) {
+
+    // BMIを算出して設定
+    double weight = health.getWeight();
+    double height = health.getHeight();
+    double bmi = weight / (height / 100.0 * height / 100.0);
+    health.setBmi(bmi);
+
+    // BMIから体型を判定して設定
+    String bodyType;
+
+    if (bmi < 18.5) {
+      bodyType = "痩せ型";
+    } else if (bmi < 25) {
+      bodyType = "普通";
+    } else {
+      bodyType = "肥満";
+    }
+
+    health.setBodyType(bodyType);
+  }
+
+}
---
+++ src/test/scope/servlet/program/HealthCheck.java
@@ -0,0 +1,58 @@
+package test.scope.servlet.program;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import test.model.program.Health;
+import test.model.program.HealthCheckLogic;
+
+/**
+ * Servlet implementation class FowardServlet
+ */
+@WebServlet("/HealthCheck")
+public class HealthCheck extends HttpServlet {
+  private static final long serialVersionUID = 1L;
+
+  /**
+   * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+   *      response)
+   */
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    // TODO Auto-generated method stub
+
+    // Foward
+    RequestDispatcher dispatcher = request
+        .getRequestDispatcher("/WEB-INF/jsp/healthCheck.jsp");
+    dispatcher.forward(request, response);
+  }
+
+  protected void doPost (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+
+    // リクエストパラメータを取得
+    String weight = request.getParameter("weight");
+    String height = request.getParameter("height");
+
+    // 入力値をプロパティに設定
+    Health health = new Health();
+    health.setHeight(Double.parseDouble(height));
+    health.setWeight(Double.parseDouble(weight));
+
+    // 健康診断を実行し結果を設定
+    HealthCheckLogic healthCheckLogic = new HealthCheckLogic();
+    healthCheckLogic.execute(health);
+
+    // リクエストスコープに保存
+    request.setAttribute("health", health);
+
+    // フォワード
+    RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/healthCheckResult.jsp");
+    dispatcher.forward(request, response);
+  }
+}
