c2e86045e36e8af0d6192272aa4e3382c6c1a0de
K-Yosuke <yosuke@192.168.1.4>
2015-05-10 22:38:04 +0900

    TODO除去

--- src/test/model/program/Health.java
+++ src/test/model/program/Health.java
@@ -6,39 +6,46 @@ import java.io.Serializable;
  * eclipse自動生成のgetter, setter専用クラス
  */
 public class Health implements Serializable {
-  private double height, weight, bmi;
-  private String bodyType;
+	
+	/**
+	 * SerialVersion
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private double height, weight, bmi;
+	
+	private String bodyType;
 
-  public double getHeight() {
-    return height;
-  }
+	public double getHeight() {
+		return height;
+	}
 
-  public void setHeight(double height) {
-    this.height = height;
-  }
+	public void setHeight(double height) {
+		this.height = height;
+	}
 
-  public double getWeight() {
-    return weight;
-  }
+	public double getWeight() {
+		return weight;
+	}
 
-  public void setWeight(double weight) {
-    this.weight = weight;
-  }
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
 
-  public double getBmi() {
-    return bmi;
-  }
+	public double getBmi() {
+		return bmi;
+	}
 
-  public void setBmi(double bmi) {
-    this.bmi = bmi;
-  }
+	public void setBmi(double bmi) {
+		this.bmi = bmi;
+	}
 
-  public String getBodyType() {
-    return bodyType;
-  }
+	public String getBodyType() {
+		return bodyType;
+	}
 
-  public void setBodyType(String bodyType) {
-    this.bodyType = bodyType;
-  }
+	public void setBodyType(String bodyType) {
+		this.bodyType = bodyType;
+	}
 
 }
